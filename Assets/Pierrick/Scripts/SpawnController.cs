5b48b08c87a56a3879ba11f5bd36c8caa59641f4
Pierrick <test@test.com>
2020-01-28 11:49:59 +0100

    Planification V0.1
    Features de base + intégration Text Mesh Pro

---
+++ Assets/Pierrick/Scripts/SpawnController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+	public static SpawnController instance;
+	[SerializeField] private Camera cam;
+	[SerializeField] private GameObject pointPrefab;
+	[SerializeField] private int spawnCount = 10;
+	[SerializeField] private string[] blockerTags;
+
+	public delegate void SpawnDelegate();
+	public event SpawnDelegate SpawnPointEvent;
+	public event SpawnDelegate SpawnComplete;
+
+	private Queue<GameObject> points;
+
+
+	//TODO: Test
+	public void RemoveAllPoints()
+	{
+		while (points.Count > 0)
+		{
+			GameObject point = points.Dequeue();
+
+			Destroy(point);
+		}
+	}
+
+	public int GetRemainingPoints()
+	{
+		return spawnCount - points.Count;
+	}
+
+
+	private void Awake()
+	{
+		if(instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		instance = this;
+
+		points = new Queue<GameObject>();
+	}
+
+	private void Start()
+	{
+		StartCoroutine(SpawnProcess());
+	}
+
+	private IEnumerator SpawnProcess()
+	{
+		while(points.Count < spawnCount)
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+				if(hit.collider != null)
+				{
+					bool hitBlocker = false;
+
+					for (int i = 0; i < blockerTags.Length; i++)
+					{
+						if(hit.transform.CompareTag(blockerTags[i]))
+						{
+							hitBlocker = true;
+						}
+					}
+
+					Debug.Log(hit.transform.tag + " " + hit.transform.name);
+
+					if (hitBlocker)
+					{
+						yield return null;
+						continue;
+					}
+						
+
+					GameObject instance = Instantiate(pointPrefab, hit.point, Quaternion.identity);
+
+					points.Enqueue(instance);
+
+					SpawnPointEvent?.Invoke();
+
+					Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red);
+				}
+			}
+
+			yield return null;
+		}
+
+		SpawnComplete?.Invoke();
+	}
+}
---
+++ Assets/Pierrick/Scripts/UI_Timeline.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Timeline : MonoBehaviour
+{
+	[SerializeField] private RectTransform timelineRect;
+	[SerializeField] private Transform pointContainer;
+	[SerializeField] private GameObject pointPrefab;
+	[SerializeField] private TextMeshProUGUI pointCountText;
+	private float x = 0;
+
+	private void Start()
+	{
+		SpawnController.instance.SpawnPointEvent += AddPoint;
+
+		pointCountText.text = SpawnController.instance.GetRemainingPoints().ToString();
+	}
+
+	private void AddPoint()
+	{
+		x += 0.1f;
+		SetPoint(x);
+
+		pointCountText.text = SpawnController.instance.GetRemainingPoints().ToString();
+	}
+
+	public void SetPoint(float value)
+	{
+		GameObject instance = Instantiate(pointPrefab, pointContainer);
+
+		instance.transform.localPosition = new Vector2((timelineRect.rect.xMin + (instance.GetComponent<RectTransform>().sizeDelta.x/2)) + 
+			value * ((timelineRect.rect.xMax - timelineRect.rect.xMin) - instance.GetComponent<RectTransform>().sizeDelta.x),
+			timelineRect.localPosition.y);
+	}
+}
