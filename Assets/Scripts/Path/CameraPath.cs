e0ce9fef7ad01286d4fbd74b721a4a726576aa4d
mathiue <mathieu.way91@gmail.com>
2020-02-09 17:42:16 +0100

    Node System

---
+++ Assets/Scripts/Path/CameraPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace oneShot
+{
+    public class CameraPath : MonoBehaviour
+    {
+        public Node CurrentNode;
+        public float pourcentage;
+        public Node previous;
+        public Node next;
+        private void Update()
+        {
+
+            if (pourcentage < 0)
+            {
+                if (previous)
+                {
+                    SetNode(previous);
+                    pourcentage = 1;
+                }
+                else
+                    pourcentage = 0;
+            }
+            else if (pourcentage > 1)
+            {
+                if (next)
+                {
+                    SetNode(next);
+                    pourcentage = 0;
+                }
+                else
+                    pourcentage = 1;
+            }
+        }
+
+        public Vector3 GetPositionAlongPath()
+        {
+            if (next)
+                return Vector3.Lerp(CurrentNode.transform.position, next.transform.position, pourcentage);
+            else
+                return CurrentNode.transform.position;
+        }
+
+        public void SetNode(Node node)
+        {
+            CurrentNode = node;
+            previous = CurrentNode.GetNeighbourNodesFromDirection(Direction.Left);
+            next = CurrentNode.GetNeighbourNodesFromDirection(Direction.Right);
+        }
+    }
+}
---
+++ Assets/Scripts/Path/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace oneShot
+{
+    public enum Direction
+    {
+        Top,
+        Right,
+        Bottom,
+        Left
+    }
+
+    [Serializable]
+    public class NeighbourNode
+    {
+        public Node node;
+        public Direction direction;
+    }
+
+    public class Node : MonoBehaviour
+    {
+        public List<NeighbourNode> neighbourNodes = new List<NeighbourNode>();
+        public Node GetNeighbourNodesFromDirection(Direction direction)
+        {
+            NeighbourNode neighbour = neighbourNodes.Find(node => node.direction == direction);
+            if (neighbour != null)
+                return neighbour.node;
+            else
+                return null;
+        }
+    }
+}
