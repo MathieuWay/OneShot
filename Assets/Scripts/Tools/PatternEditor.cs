f65f7562e3337fae29afc34b9888528641abbbc4
mathieuway91 <mathieu.way91@gmail.com>
2020-04-21 10:58:40 +0200

    Inspector GUI Pattern Editor

--- Assets/Scripts/Tools/PatternEditor.cs
+++ Assets/Scripts/Tools/PatternEditor.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 #if UNITY_EDITOR
+using NaughtyAttributes.Editor;
 using UnityEditor;
+using UnityEditorInternal;
+
 [CustomEditor(typeof(oneShot.Pattern), true)]
 public class PatternEditor : Editor
 {
+    ReorderableList _patternStepsReorderableList;
+
     private void OnSceneGUI()
     {
         oneShot.Pattern pattern = (oneShot.Pattern)target;
@@ -19,6 +24,56 @@ public class PatternEditor : Editor
                 }
             }
         }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        DrawPropertiesExcluding(serializedObject, new string[] { "steps"});
+        _patternStepsReorderableList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void OnEnable()
+    {
+        SerializedProperty patternsStepsProperty = serializedObject.FindProperty("steps");
+        _patternStepsReorderableList = new ReorderableList(serializedObject, patternsStepsProperty);
+        _patternStepsReorderableList.elementHeight = EditorGUIUtility.singleLineHeight * 3 + 10f;
+        _patternStepsReorderableList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            SerializedProperty patternStepsProperty = serializedObject.FindProperty("steps").GetArrayElementAtIndex(index);
+            Rect propertyRect = rect;
+            //STEP TYPE
+            propertyRect.height = EditorGUIUtility.singleLineHeight;
+            SerializedProperty patternStepTypeProperty = patternStepsProperty.FindPropertyRelative("type");
+            EditorGUI.PropertyField(propertyRect, patternStepTypeProperty);
+            //START TIME
+            propertyRect.y += EditorGUIUtility.singleLineHeight;
+            SerializedProperty patternStartTimeProperty = patternStepsProperty.FindPropertyRelative("startTime");
+            EditorGUI.LabelField(propertyRect, "Début de la Step:", patternStartTimeProperty.floatValue.ToString());
+
+            oneShot.StepType stepType = (oneShot.StepType)patternStepTypeProperty.intValue;
+            switch (stepType)
+            {
+                case oneShot.StepType.Idle:
+                    propertyRect.y += EditorGUIUtility.singleLineHeight;
+                    SerializedProperty patternTargetPosProperty = patternStepsProperty.FindPropertyRelative("duration");
+                    EditorGUI.PropertyField(propertyRect, patternTargetPosProperty);
+                    break;
+                case oneShot.StepType.Move:
+                    propertyRect.y += EditorGUIUtility.singleLineHeight;
+                    SerializedProperty patternDurationProperty = patternStepsProperty.FindPropertyRelative("targetPos");
+                    EditorGUI.PropertyField(propertyRect, patternDurationProperty);
+                    break;
+                case oneShot.StepType.Anim:
+                    propertyRect.y += EditorGUIUtility.singleLineHeight;
+                    SerializedProperty patternClipProperty = patternStepsProperty.FindPropertyRelative("clip");
+                    EditorGUI.PropertyField(propertyRect, patternClipProperty);
+                    break;
+                default:
+                    break;
+            }
+        };
     }
 }
 #endif
