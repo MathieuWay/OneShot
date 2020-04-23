b84b452357c3bb2f26148a8af57edd5bf19a7927
PierrickMkDev <pierrick.maczkowiak@edu.devinci.fr>
2020-04-23 17:57:54 +0200

    First Chrona Animation Integration

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -11,6 +11,7 @@ namespace oneShot
 
 		[SerializeField] private Transform centerPivot = null;
 		[SerializeField] private GameObject pivot = null;
+		[SerializeField] private Animator chronaAnim = null;
 		public Transform CenterPivot { get => centerPivot; }
 		public bool IsDead { get; private set; }
 		private Animator anim;
@@ -43,6 +44,26 @@ namespace oneShot
 		private void Start()
 		{
 			anim = GetComponentInChildren<Animator>();
+
+			ComboController.Instance.ComboSuccessEvent += PlayAnim;
+		}
+
+		private void PlayAnim(AttackName attackName)
+		{
+			switch(attackName)
+			{
+				case AttackName.SpiralAttack:
+					chronaAnim.SetTrigger("Spiral");
+					break;
+
+				case AttackName.SlashAttack:
+					chronaAnim.SetTrigger("Slash");
+					break;
+
+				case AttackName.ThrustAttack:
+					chronaAnim.SetTrigger("Thrust");
+					break;
+			}
 		}
 	}
 }
