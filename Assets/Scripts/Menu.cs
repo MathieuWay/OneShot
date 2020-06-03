ad0a709fa53a06d96346890ec9516ab216eab519
PierrickMkDev <pierrick.maczkowiak@edu.devinci.fr>
2020-06-03 10:51:03 +0200

    Menu V1

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace oneShot
+{
+	public class Menu : MonoBehaviour
+	{
+		private void Update()
+		{
+			if(Gamepad.Instance.ButtonDownStart)
+			{
+				LevelLoader.Instance.LoadNextLevel();
+				SoundManager.Instance.PlaySound("start_carnage");
+			}
+		}
+	}
+}
